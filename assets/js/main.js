bf51738982e5f86c874604458b4aa3612a4cc58a
kristianAndersen <krilleandersen@gmail.com>
2021-01-15 11:59:34 +0000

    addet a drag function

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,6 +51,50 @@ if( window.devicePixelRatio !== 1 ){
 
 	window.addEventListener("resize", canvasResize);
 
-	canvasResize();
+    canvasResize();
+    
+
+/**Drag the stuff arround */
+/*Thanks to Cyrielle her version of drag vas better than mine 
+https://codepen.io/Milky-box/pen/GRoexRz
+*/    
+ dragbar(document.querySelector('.dragbar'))
+
+    function dragbar(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.parentElement.style.top = (elmnt.parentElement.offsetTop - pos2) + "px";
+    elmnt.parentElement.style.left = (elmnt.parentElement.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+    
+    
 
 });
